98a66767f83f2f6f964280d7a2b8430b2351a745
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-06-22 21:07:54 -0400

    added font family and container to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      sans: ['Anek Latin', 'sans-serif'],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class', // only generate classes
+    }),
+  ],
 };
