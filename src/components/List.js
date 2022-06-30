84272e58d78ef609784de132f023e8e157c0ed4c
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-06-29 20:13:25 -0400

    passed down props to list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
-export default function List({ taskdata }) {
+import { TiDelete } from 'react-icons/ti';
+import { AiFillEdit } from 'react-icons/ai';
+export default function List({ taskdata, handleRemove, handleEdit }) {
   return (
     <ul className="mt-6 mr-3">
       {taskdata.map((item) => (
         <li key={item.id} className="li">
           {item.taskval}
+          <button
+            type="button"
+            onClick={() => handleRemove(item.id)}
+            className="btn">
+            <TiDelete />
+          </button>
+          <button
+            type="button"
+            onClick={() => handleEdit(item.id)}
+            className="btn">
+            <AiFillEdit />
+          </button>
         </li>
       ))}
     </ul>
